35eea7112bff8adadb50f9ca433a32fdd1415683
Joel Castro <joey-92@live.com>
2023-04-17 09:00:18 -0600

    Modified files.

---
+++ Chain/ChainFunctional.py
@@ -0,0 +1,21 @@
+from typing import Any, Callable, List, Tuple, Union
+
+from ChainBase import ChainBase
+
+
+class ChainFunctional(ChainBase):
+
+    @staticmethod
+    def _filter(f: Callable):
+        def apply(seq: Any):
+            return list(filter(f, seq))
+
+        return apply
+
+    @staticmethod
+    def _map(f: Callable):
+        def apply(seq: Any):
+            return list(map(f, seq))
+
+        return apply
+
